98ef3b979805fc4d984327ad7b0c6e48f8885e88
lavallone <lavalle.1838492@studenti.uniroma1.it>
2022-11-16 18:35:17 +0100

    y

--- Homework_01/main.py
+++ Homework_01/main.py
@@ -11,15 +11,17 @@ if __name__ == "__main__":
 	parser.add_argument('-i','--inputFile', help='Filename to run BookNLP on', required=True)
 	parser.add_argument('-o','--outputFolder', help='Folder to write results to', required=True)
 	parser.add_argument('--id', help='ID of text (for creating filenames within output folder)', required=True)
+	parser.add_argument('--model_size', help='Size of the pretrained model', required=True)
 
 	args = vars(parser.parse_args())
 
 	inputFile=args["inputFile"]
 	outputFolder=args["outputFolder"]
 	idd=args["id"]
+	model_size=args["model_size"]
 
 	print("tagging %s" % inputFile)
 
-	model_params={"pipeline":"entity", "model":"big",} # we exploit only the named entity tagger of the BookNLP pipeline!
+	model_params={"pipeline":"entity", "model":model_size,} # we exploit only the named entity tagger of the BookNLP pipeline!
 	booknlp = BookNLP(model_params)
 	booknlp.process(inputFile, outputFolder, idd)
