cfca88dc4dc9e86272f553582f603c767420be78
lavallone <lavalle.1838492@studenti.uniroma1.it>
2022-11-18 10:13:01 +0100

    s

--- Homework_01/main.py
+++ Homework_01/main.py
@@ -21,23 +21,17 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--inputFairyFolder', help='Folder containing Fairy Tales', required=True)
 	parser.add_argument('--inputShortFolder', help='Folder containing Short Stories', required=True)
-	#parser.add_argument('-i','--inputFile', help='Filename to run BookNLP on', required=True)
-	#parser.add_argument('-o','--outputFolder', help='Folder to write results to', required=True)
-	#parser.add_argument('--id', help='ID of text (for creating filenames within output folder)', required=True)
+	parser.add_argument('--stories_selection', help='Selection of stories to process', required=False)
 	parser.add_argument('--model_size', help='Size of the pretrained model', required=True)
 
 	args = vars(parser.parse_args())
-
-	#stories_list = args["stories_list"]
+	print(args["stories_selection"])
 	inputFairyFolder = args["inputFairyFolder"]
 	inputShortFolder = args["inputShortFolder"]
 	fairy_tales_list = [inputFairyFolder+s for s in os.listdir(inputFairyFolder)]
 	short_stories_list = [inputShortFolder+s for s in os.listdir(inputShortFolder)]
 	stories_list = fairy_tales_list + short_stories_list
-	#inputFile=args["inputFile"]
-	#outputFolder=args["outputFolder"]
 	outputFolder="/content/results/" # here I'm going to save the computed named entities!
-	#idd=args["id"]
 	model_size=args["model_size"]
  
 	model_params={"pipeline":"entity", "model":model_size,} # we exploit only the named entity tagger of the BookNLP pipeline!
